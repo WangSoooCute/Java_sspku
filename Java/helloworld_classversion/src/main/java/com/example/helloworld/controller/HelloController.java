804c3e32be6eed084a2713aa6e960d8d5dd8d83d
WangSoooCute <38801071+WangSoooCute@users.noreply.github.com>
2024-11-13 21:41:18 +0800

    更改了“1、后端部分”的Swagger部分

--- Java/helloworld_classversion/src/main/java/com/example/helloworld/controller/HelloController.java
+++ Java/helloworld_classversion/src/main/java/com/example/helloworld/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.helloworld.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @ApiOperation("获取用户")
     @GetMapping("/hello")
     public String hello(){
         return "Hello world!";
