5574a335057dc6507ba16ab276086ea4a398ea17
WangSoooCute <38801071+WangSoooCute@users.noreply.github.com>
2024-11-22 15:46:40 +0800

    1

---
+++ Vue/component-demo_classversion/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--    <movie title="名字"></movie>-->
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import Movie from "@/components/Movie.vue";
+import Hello from "@/components/Hello.vue";
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    // Movie
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/component-demo_classversion/src/components/Movie.vue
@@ -0,0 +1,21 @@
+<!--<template>-->
+<!--  <div >-->
+<!--    <h1>{{title}}</h1>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "Movie",-->
+<!--  props: ['title'],-->
+<!--  data: function() {-->
+<!--    return {-->
+<!--      title:"金刚狼"-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
