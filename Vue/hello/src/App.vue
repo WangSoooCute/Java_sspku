62cb62c2704088219db9f56da7680e2dad4588d5
WangSoooCute <38801071+WangSoooCute@users.noreply.github.com>
2024-11-22 15:52:34 +0800

    1

---
+++ Vue/hello/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Hello v-for="movie in movies" :key="movie.id" :title="movie.title" :rating="movie.rating"></Hello>
+  <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="Pick a day"
+        :size="size"
+      />
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+export default {
+  name: 'App',
+  data:function(){
+    return {
+      movies:[
+        {id:1,title:"电影1",rating:8.7},
+        {id:2,title:"电影1",rating:8.7},
+        {id:3,title:"电影1",rating:8.7},
+      ]
+    }
+  },
+  components: {
+    Hello
+},
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/hello/src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>{{message}}</h1> 
+    <button @click="fun">点击</button> 
+</template>
+
+<script>
+export default{
+    name:"Hello",
+    components:{
+    },
+    data:function(){
+        return {
+            message:"hello world"
+        }
+    },
+    methods:{
+        fun(){
+            alert("hello")
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color: red;
+}
+</style>
