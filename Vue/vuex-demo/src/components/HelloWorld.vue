3ab9f1ba5fe0fadff2894beb7c1f77fe51e5857d
WangSoooCute <wzixuan1@gmail.com>
2024-11-26 21:53:45 +0800

    Vuex

---
+++ Vue/vuex-demo/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <!-- {{this.$store.state.count}} -->
+    {{count}}
+    <button @click="add">+1</button>
+
+    <ul>
+      <li v-for="todo in doneTodos" :key="todo.id">{{todo.text}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState,mapGetters } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  computed:{
+      ...mapState([
+      'count','todos'
+      ]),
+      ...mapGetters([
+        'doneTodos'
+      ])
+  },
+  methods:{
+    add(){
+        this.$store.commit("increment",2)
+    }
+  }
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/vuex-demo_classversion/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <button @click="increment">+1</button>
+    <p>{{ $store.state.count }}</p>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  methods: {//在export default里面
+    ...mapMutations(['increment'])
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
